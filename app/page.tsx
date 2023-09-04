b396e7e53388e95529d509c581c07cec6934525d
Tianqi Yao <yaotianqi6666@gmail.com>
2023-09-04 21:54:23 +0000

    card v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
+import Card from "@/components/card";
+import img from "../public/images/kenan.jpg"
+
 
 export default function Home() {
-  return <>Cardlists</>;
+  return <div className="cardBar">
+    <Card title="manga" content="kenan" image={img.src} />
+    <Card title="manga" content="kenan" image={img.src} />
+    <Card title="manga" content="kenan" image={img.src} />
+    <Card title="manga" content="kenan" image={img.src} />
+  </div>;
 }
