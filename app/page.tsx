d3312bebbd8656a59d5ae63cd1643f49ed52ae7d
Aoli <939677612@qq.com>
2023-09-04 16:32:43 -0500

    0904
    basic scratch

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
-import Image from "next/image";
+import "./globals.css";
 
 export default function Home() {
-  return <>Cardlists</>;
+  return (
+    <div>
+      <div className="bg-slate-50 border-2 border-solid border-slate-300 mt-4">
+        <h2>Image Processing</h2>
+        <div className="">CardLists</div>
+      </div>
+      <div className="bg-slate-50 border-2 border-solid border-slate-300  mt-4">
+        <h2>DevTools</h2>
+        <div className="">CardLists</div>
+      </div>
+    </div>
+  );
 }
