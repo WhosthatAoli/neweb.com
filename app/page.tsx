40a629e2714a780ab2ca58861635aa469aa47481
Aoli <939677612@qq.com>
2023-09-06 13:16:27 -0500

    0906
    structure correction

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import Card from "@/components/card";
 import img from "../public/images/kenan.jpg";
 import { websites } from "../constant";
+import Banner from "@/components/banner";
 
 export default function Home() {
   return (
     <div>
+      <Banner />
       <div className="collection bg-slate-300 mt-4">
         <div className="text-2xl pl-6">DevTools</div>
         <div className="cardBar flex justify-between pl-4 pr-4">
