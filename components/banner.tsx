174e22419fa75745586085b39c1bd817d3f2ceb7
jiayuegou <65645659+jiayuegou@users.noreply.github.com>
2023-09-07 20:26:41 -0500

    change banner layout

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,20 +1,21 @@
 import * as React from "react";
 import "../app/globals.css";
 import { banners } from "../constant";
-import { categories } from "../constant";
+import { features } from "process";
 
 interface BannerProps {}
 
 const Banner: React.FunctionComponent<BannerProps> = (props) => {
   return (
     <div>
-      <div className="w-full h-40 bg-gray-400 p-14">
+      <div className="w-full h-40 bg-gray-400 p-8">
         <div className="flex justify-start items-center gap-10">
-          {
-            categories.map(item => (
+          feature
+          {/* {
+            features.map(item => (
               <div>{item}</div>
             ))
-          }
+          } */}
         </div>
         {/* {
           banners[0]
