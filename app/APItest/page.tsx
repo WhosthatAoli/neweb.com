b5b02bce0b66c5446fcda64dccba66f3f2ab1dc4
Tianqi Yao <yaotianqi6666@gmail.com>
2023-09-09 22:35:43 -0500

    fix feature split bug

--- app/APItest/page.tsx
+++ app/APItest/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
           <div className="text-2xl font-bold mt-2 ml-6 mb-2">{feature}</div>
           <div className="cardBar flex overflow-auto gap-4 pt-4 pb-4 ml-6 mr-4">
             {websites
-              .filter((site) => site.feature.split(",").includes(feature))
+              .filter((site) => site.feature?.split(",").includes(feature))
               .map((filteredSite, index) => (
                 <Card
                   key={index}
