0b5fd30606e459cc9f2f9d4deb6b72abc149fd12
Aoli <939677612@qq.com>
2023-09-14 16:39:42 -0500

    0914-5
    add login,register

--- app/(user)/Register/page.tsx
+++ app/(user)/Register/page.tsx
@@ -7,7 +7,10 @@ import Navbar from "@/components/navbar";
 export default function Login() {
   const [eamil, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {};
+  const [password2, setPassword2] = useState("");
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    console.log("handleSubmit");
+  };
 
   return (
     <>
@@ -21,7 +24,7 @@ export default function Login() {
               alt="Your Company"
             />
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
+              Sign up your account
             </h2>
           </div>
 
@@ -60,14 +63,6 @@ export default function Login() {
                   >
                     Password
                   </label>
-                  <div className="text-sm">
-                    <a
-                      href="#"
-                      className="font-semibold text-indigo-600 hover:text-indigo-500"
-                    >
-                      Forgot password?
-                    </a>
-                  </div>
                 </div>
                 <div className="mt-2">
                   <input
@@ -83,22 +78,44 @@ export default function Login() {
               </div>
 
               <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Repeat Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    onChange={(e) => setPassword2(e.target.value)}
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
                 <button
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Sign in
+                  Sign up
                 </button>
               </div>
             </form>
 
             <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{" "}
+              Already a member?{" "}
               <a
-                href="/Register"
+                href="/Login"
                 className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
               >
-                Sign up now
+                Sign in now
               </a>
             </p>
           </div>
