f824f76d098e42f1c78904cfe643c05ce106944d
Aoli <939677612@qq.com>
2023-09-14 16:39:46 -0500

    Update route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,48 +1,62 @@
-import { NextResponse } from 'next/server'
-import { GetWebsites, AddWebsites, DeleteWebsitesFromCategory } from "../../api/route";
-import { QueryResult, QueryResultRow } from '@vercel/postgres';
-import {takeScreenshot} from "../../scripts/updateScreenshots.js"
+import { NextResponse } from "next/server";
+import {
+  GetWebsites,
+  AddWebsites,
+  DeleteWebsitesFromWebsites,
+} from "../../api/route";
+import { QueryResult, QueryResultRow } from "@vercel/postgres";
+import { takeScreenshot } from "../../scripts/updateScreenshots.js";
 import path from "path";
 
 export async function GET() {
-    const websites = await GetWebsites();
-    const websitesData = websites.rows;
-    return NextResponse.json({ websitesData }, { status: 200 })
+  const websites = await GetWebsites();
+  const websitesData = websites.rows;
+  return NextResponse.json({ websitesData }, { status: 200 });
 }
 
-
 export async function POST(req: Request) {
-    if (req.method === 'POST') {
-        try {
-            const formData = await req.json();
-            console.log("formData: ", formData);
-            if (formData.img === "") {
-                const isNull = await takeScreenshot(formData.url, formData.name);
-                if (isNull !== null) {
-                    formData.img = path.resolve("/screenshots/" + formData.name + ".png");
-                }
-            }
-            const websites = await AddWebsites(formData);
-            const websitesData = (websites as QueryResult<QueryResultRow>).rows;
-            return NextResponse.json({ websitesData }, { status: 200 });
-        } catch (error) {
-            return NextResponse.json({ error: 'An error occurred while adding the website.' }, { status: 500 });
+  if (req.method === "POST") {
+    try {
+      const formData = await req.json();
+      console.log("formData: ", formData);
+      if (formData.img === "") {
+        const isNull = await takeScreenshot(formData.url, formData.name);
+        if (isNull !== null) {
+          formData.img = path.resolve("/screenshots/" + formData.name + ".png");
         }
-    } else {
-        return NextResponse.json({ error: 'Method not allowed.' }, { status: 405 });
+      }
+      const websites = await AddWebsites(formData);
+      const websitesData = (websites as QueryResult<QueryResultRow>).rows;
+      return NextResponse.json({ websitesData }, { status: 200 });
+    } catch (error) {
+      return NextResponse.json(
+        { error: "An error occurred while adding the website." },
+        { status: 500 }
+      );
     }
+  } else {
+    return NextResponse.json({ error: "Method not allowed." }, { status: 405 });
+  }
 }
 
 export async function DELETE(req: Request) {
-    if (req.method === 'DELETE') {
-        try {
-            const { category, websiteName } = await req.json();
-            await DeleteWebsitesFromCategory({ category, websiteName });
-            return NextResponse.json({ message: 'Website successfully deleted from category.' }, { status: 200 });
-        } catch (error) {
-            return NextResponse.json({ error: 'An error occurred while deleting the website from category.' }, { status: 500 });
-        }
-    } else {
-        return NextResponse.json({ error: 'Method not allowed.' }, { status: 405 });
+  if (req.method === "DELETE") {
+    try {
+      const { websiteName } = await req.json();
+      await DeleteWebsitesFromWebsites({ websiteName });
+      return NextResponse.json(
+        { message: "Website successfully deleted from category." },
+        { status: 200 }
+      );
+    } catch (error) {
+      return NextResponse.json(
+        {
+          error: "An error occurred while deleting the website from category.",
+        },
+        { status: 500 }
+      );
     }
+  } else {
+    return NextResponse.json({ error: "Method not allowed." }, { status: 405 });
+  }
 }
