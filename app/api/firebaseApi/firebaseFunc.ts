4d5b9ebcd9eec5a51745b956ff04e85c7f44eb3c
Tianqi Yao <yaotianqi6666@gmail.com>
2023-09-30 16:54:00 -0500

    del alert

--- app/api/firebaseApi/firebaseFunc.ts
+++ app/api/firebaseApi/firebaseFunc.ts
@@ -102,7 +102,6 @@ const updateFormData = async (formData: FormDataType, imgFile: File | null) => {
         }
         // 更新数据库
         await updateDatabase(formData);
-        alert("Website added successfully.");
         return true;
     } catch (error) {
         console.error("error: ", error);
